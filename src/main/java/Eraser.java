d7cb4207466461ead0cf212a1bab80db06248406
Austin.Kucera01 <austin.kucera01@cardinalhealth.com>
2022-09-19 13:40:25 -0400

    extract eraser methods

--- src/main/java/Eraser.java
+++ src/main/java/Eraser.java
@@ -15,30 +15,33 @@ public class Eraser {
     }
 
     public void erase(Paper paper, String textToErase) {
-        String text = paper.getText();
-        String replacementText = textToErase;
-
-        // Finds last index of occurrence of textToErase String
-        int lastIndex = text.lastIndexOf(textToErase);
-
-        // If string found on paper, grab everything before and after that occurrence and store substrings in a new variable
-        if (lastIndex > -1) {
-            String beginString = text.substring(0, lastIndex);
-            String endString = text.substring(lastIndex + textToErase.length());
-
-            // Turn textToErase into an array of characters
-            char[] arrayOfCharacters = textToErase.toCharArray();
-
-            // Starting at the last index of the array, check that the char is not whitespace and if eraser has durability enough to erase
-            for (int i = arrayOfCharacters.length - 1; i >= 0; i--) {
-                if (!Character.isWhitespace(arrayOfCharacters[i])) {
-                    if (durability >= 1) {
-                        replacementText = replacementText.substring(0, i) + " " + replacementText.substring(i + 1);
-                        durability -= 1;
-                    }
-                }
-            }
-            paper.setText(beginString + replacementText + endString);
+        int lastOccurrenceOfTextToErase = paper.getText().lastIndexOf(textToErase);
+        if (lastOccurrenceOfTextToErase > -1) {
+            doErase(paper, textToErase, lastOccurrenceOfTextToErase);
         }
+    }
+
+    private void doErase(Paper paper, String textToErase, int lastIndex) {
+        String replacementText = textToErase;
+        String beginString = paper.getText().substring(0, lastIndex);
+        String endString = paper.getText().substring(lastIndex + textToErase.length());
+
+        char[] charactersToErase = textToErase.toCharArray();
+
+        // Starting at the last index of the array, check that the char is not whitespace and if eraser has durability enough to erase
+        for (int i = charactersToErase.length - 1; i >= 0; i--) {
+            replacementText = eraseCharacter(replacementText, charactersToErase, i);
+        }
+        paper.setText(beginString + replacementText + endString);
+    }
+
+    private String eraseCharacter(String replacementText, char[] charactersToErase, int i) {
+        if (!Character.isWhitespace(charactersToErase[i])) {
+            if (durability >= 1) {
+                replacementText = replacementText.substring(0, i) + " " + replacementText.substring(i + 1);
+                durability -= 1;
+            }
+        }
+        return replacementText;
     }
 }
